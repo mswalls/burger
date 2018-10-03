a8cd2b9036201abab7c722c2a45afbbe18421f97
mswalls <mswalls@rocketmail.com>
2018-10-03 15:41:04 -0400

    initial commit

---
+++ config/orm.js
@@ -0,0 +1,9 @@
+var connection = require("./connection.js");
+
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('New Bacon-ings', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Salvador Cauliflower Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("She's a Super Leek Burger", false);
